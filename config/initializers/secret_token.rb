2ea989a6646f383edf2e4a0a7514b0b6b732561a
Juan Alvarado <juanalblue@gmail.com>
2014-04-19 20:24:27 -0700

    Initial empty rails app with the aws-sdk gem added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Federator::Application.config.secret_key_base = 'd31bb0c89242daf01639484924fa0390a31a06355e8fffae4e1cd7438b9a7980714da0ac2cd24a546204102eef0f5a6edca0ec9fcb5aab324d81c9b31edec715'
